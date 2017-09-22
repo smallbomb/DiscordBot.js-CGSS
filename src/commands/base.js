db568b46cae9b713de92175e211eaa052bd64762
smallbomb <rockon590+git@gmail.com>
2017-09-22 15:13:43 +0800

    Add base.js for sample
    example command struct

---
+++ src/commands/base.js
@@ -0,0 +1,12 @@
+module.exports = function (client, message) { 
+  // your write command code...
+  
+};
+
+function Help(message) {
+  message.channel.send(
+    "```\n" +
+    "command " + message.content.split(" ")[0] + "\n" +
+    "```"
+  );
+}
