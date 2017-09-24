ca027bb8ace9421bf0552bdc9521615d4a06dd01
smallbomb <rockon590+git@gmail.com>
2017-09-24 11:20:56 +0800

    Update client.js
    Improve readability

--- src/client/client.js
+++ src/client/client.js
@@ -3,22 +3,26 @@ const CommandRegistry = require('./registry.js');
 class Client extends discord.Client {
   constructor() {
     super();
+
     this.commandarray = [];
-    this.registry = function (path) {
-      CommandRegistry(this, path);
-    };
-    this.messagesParse = function (message) {
-      let command = message.content.split(" ")[0];
-      for (let i = 0; i < this.commandarray.length; i++) {
-        if (command.toLowerCase() === this.commandarray[i].commandname.toLowerCase()) {
-          let Run = require(this.commandarray[i].path);
-          Run(this, message);
-          delete require.cache[require.resolve(this.commandarray[i].path)];
-          break;
-        }
-      }
-    };
+
+    this.registry = function (path) { CommandRegistry(this, path) };
+
+    this.messagesParse = function (message) { MessagesParse(this, message) };
+  }
+}
+
+function MessagesParse(Client, message) {
+  let command = message.content.split(" ")[0];
+  for (let i = 0; i < Client.commandarray.length; i++) {
+    if (command.toLowerCase() === Client.commandarray[i].commandname.toLowerCase()) {
+      let Run = require(Client.commandarray[i].path);
+      Run(Client, message);
+      delete require.cache[require.resolve(Client.commandarray[i].path)];
+      break;
+    }
   }
 }
 
 module.exports = Client;
+
