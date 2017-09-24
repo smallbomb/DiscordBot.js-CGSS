12be5c1e2bb9c2b6123f8cf432ef4bacf6554799
smallbomb <rockon590+git@gmail.com>
2017-09-24 10:58:57 +0800

    Update ping command
    Real sending message latency time

--- src/commands/user/ping.js
+++ src/commands/user/ping.js
@@ -1,3 +1,4 @@
-module.exports = function (client, message) {
-    message.channel.send(client.ping);
+module.exports = async function (client, message) {
+  const m = await message.channel.send("Ping?");
+  m.edit(`${m.createdTimestamp - message.createdTimestamp}ms.`);
 };
