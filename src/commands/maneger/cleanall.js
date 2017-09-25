11b23999b078ff8de0aac77b4486e1809eb3a9b9
smallbomb <rockon590+git@gmail.com>
2017-09-25 15:38:03 +0800

    BUGFIX modify varible name

--- src/commands/maneger/cleanall.js
+++ src/commands/maneger/cleanall.js
@@ -1,22 +1,25 @@
 const ownerID = require('../../certificate.json').ownerID;
-const two_week_milliseconds = 60 * 60 * 24 * 7 * 2 * 1000 ;
+const two_week_milliseconds = 60 * 60 * 24 * 7 * 2 * 1000;
 
 module.exports = async function (client, message) {
   if (message.author.id === ownerID) {
     if (message.content.split(" ")[1] === undefined) {
       let msg_array = await message.channel.fetchMessages({ limit: 100 });
-      msg_array = msg_array.array().filter(old_m => old_m.id !== message.id && Date.now() - old_m.createdTimestamp - < two_week_seconds);
+      msg_array = msg_array.array().filter(old_m => old_m.id !== message.id && Date.now() - old_m.createdTimestamp < two_week_milliseconds);
       if (msg_array.length == 1) msg_array.map(async m => await m.delete().catch(console.error));
       else if (msg_array.length >= 2) await message.channel.bulkDelete(msg_array, true).catch(console.error);
       else;
     }
     else if (message.content.split(" ")[1] === "-f") {
-      let msg_array = await message.channel.fetchMessages({ limit: message.content.split(" ")[2] === undefined ? 10 : message.content.split(" ")[2] });
+      let msg_array = await message.channel.fetchMessages({
+        limit: message.content.split(" ")[2] === undefined ? 10 : message.content.split(" ")[2]
+      });
       msg_array = msg_array.array().filter(m => m.id !== message.id);
       msg_array.map(async m => await m.delete().catch(console.error));
     }
     else
       return Help(message);
+
     await message.delete(500);
     message.channel.send("clean done");
   }
--- src/commands/user/clean.js
+++ src/commands/user/clean.js
@@ -4,7 +4,7 @@ const two_week_milliseconds = 60 * 60 * 24 * 7 * 2 * 1000;
 module.exports = async function (client, message) {
   if (message.author.id === ownerID) {
     let msg_array = await message.channel.fetchMessages({ limit: 100 });
-    msg_array = msg_array.array().filter(old_m => old_m.author.id === client.user.id && old_m.id !== message.id && Date.now() - old_m.createdTimestamp < two_week_seconds);
+    msg_array = msg_array.array().filter(old_m => old_m.author.id === client.user.id && old_m.id !== message.id && Date.now() - old_m.createdTimestamp < two_week_milliseconds);
     if (msg_array.length == 1) msg_array.map(async m => await m.delete().catch(console.error));
     else if (msg_array.length >= 2) await message.channel.bulkDelete(msg_array, true).catch(console.error);
 
