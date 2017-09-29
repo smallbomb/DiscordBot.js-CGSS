9f782f4a7caa9b543124f830eb291ec67a8b8f5b
smallbomb <rockon590+git@gmail.com>
2017-09-30 07:30:25 +0800

    add sample pictures

--- src/commands/user/draw.js
+++ src/commands/user/draw.js
@@ -9,8 +9,8 @@ module.exports = function (client, message) {
   // let images2 = images("./src/data/images/lots/大凶.png");
   // let merge_images = images(images1.width() + 10 + images2.width(), images1.height()).draw(images1, 0, 0).draw(images2, images1.width()+10, 0);
   // merge_images.save("output.jpg");
-  // return ;
-  for (let i = 0; i < userOption.drawcount; i++) {
+  return ;
+  for (let i = 0; i < userOpt.drawcount; i++) {
     card = GetACard(userOpt.pool_index, "SSR");
   
 
@@ -29,12 +29,6 @@ function Help(message) {
 }
 
 function GetACard(index, str) {
-  let BounsSSRprob = parseFloat(jsonObj.BounsSSRprob) * 100;
-  let BounsSRprob = parseFloat(jsonObj.BounsSRprob) * 100;
-  let SSRprob = parseFloat(jsonObj.SSRprob) * 100;
-  let SRprob = parseFloat(jsonObj.SRprob) * 100;
-  let Rprob = parseFloat(jsonObj.Rprob) * 100; // SSRprob + SRprob + Rprob 1~10000
-
   let cardType = undefined;
   if (str === "SSR")
     cardType = Math.floor(Math.random() * 100 + 15);
@@ -50,8 +44,9 @@ function requireUncached(module) {
 }
 
 function DrawOtionParser(message) {
+  let r = /^[0-9]*[1-9][0-9]*$/; // N: natural number
   // default value
-  let drawcount = 1;
+  let drawcount = 10;
   let pool_index = jsonObj.cardpool.length;
   // =============
   let parse = optset(("garbage_str " + message.content).split(/ +/g), 'hi:c:');
@@ -61,10 +56,15 @@ function DrawOtionParser(message) {
         Help(message);
         return undefined;
       case 'i':
+        if(r.test(opt.arg) && opt.arg > jsonObj.cardpool.length) {
+          Help(message);
+          return undefined;
+        }
         pool_index = opt.arg;
         break;
       case 'c':
-        drawcount = opt.arg;
+        if (r.test(opt.arg) && opt.arg <= 10) drawcount = parseInt(opt.arg);
+        else return undefined ;
         break;
       default:
         Help(message);
