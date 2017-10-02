21b57ef6b592875b29cd21208c3bafb8cfee14d9
smallbomb <rockon590+git@gmail.com>
2017-10-02 16:27:27 +0800

    format code style

--- src/commands/user/draw.js
+++ src/commands/user/draw.js
@@ -11,9 +11,10 @@ module.exports = function (client, message) {
   let userOpt = undefined;
   let count = 0;
   if ((userOpt = DrawOtionParser(message)) === undefined) return;
+  
   cardpool = setCardPool(userOpt.pool_index);
   for (let i = 0; i < userOpt.count; i++) {
-    if (i === 9 && !card.find(c => {return c.type === "SSR" || c.type === "SR"})) // check one SR or SSR at least? 
+    if (i === 9 && !card.find(c => { return c.type === "SSR" || c.type === "SR" })) // check one SR or SSR at least? 
       card.push(GetACard(cardpool, "SR"));
     else
       card.push(GetACard(cardpool));
@@ -27,9 +28,9 @@ module.exports = function (client, message) {
 function Help(message) {
   message.channel.send(
     "```\n" +
-    "command " + message.content.split(" ")[0] + "\n" +
+    "command " + message.content.split(" ")[0] + "(暫時把計算和紀錄砍掉..回復時間未知,目前只能選單色池1~3)\n" +
     "   -h       :help\n" +
-    "   -c chose :數字, 選一個卡池(default: " + drawJson.cardpool.length + "號卡池)\n" + 
+    "   -c chose :數字, 選一個卡池(default: " + drawJson.cardpool.length + "號卡池)\n" +
     "   -n num   :數字, 數量1~10(default: 10)\n" +
     "   -i info  :卡池資訊\n" +
     "      info  :\"all\" or \"數字編號\"\n" +
@@ -43,29 +44,26 @@ function GetACard(cardpool, str) {
     name: undefined
   };
   let prob = undefined;
-  let SSRprob = parseInt(drawJson.SSRprob)*100;
-  let SRprob = parseInt(drawJson.SRprob)*100;
-  let Rprob = parseInt(drawJson.Rprob)*100;
+  let SSRprob = parseInt(drawJson.SSRprob) * 100;
+  let SRprob = parseInt(drawJson.SRprob) * 100;
+  let Rprob = parseInt(drawJson.Rprob) * 100;
   if (str === "SSR")
     prob = Math.floor(Math.random() * SSRprob) + 1;
   else if (str === "SR")
     prob = Math.floor(Math.random() * SRprob) + SSRprob + 1;
   else if (str === "R")
     prob = Math.floor(Math.random() * Rprob) + SSRprob + SRprob + 1;
-  else 
+  else
     prob = Math.floor(Math.random() * (SSRprob + SRprob + Rprob)) + 1;
-  if (prob <= SSRprob)
-  {
+  if (prob <= SSRprob) {
     Card.type = "SSR";
     Card.name = cardpool.SSR[Math.floor(Math.random() * cardpool.SSR.length)];
   }
-  else if (prob <= SSRprob + SRprob)
-  {
+  else if (prob <= SSRprob + SRprob) {
     Card.type = "SR";
     Card.name = cardpool.SR[Math.floor(Math.random() * cardpool.SR.length)];
   }
-  else if (prob <= SSRprob + SRprob + Rprob)
-  {
+  else if (prob <= SSRprob + SRprob + Rprob) {
     Card.type = "R";
     Card.name = cardpool.R[Math.floor(Math.random() * cardpool.R.length)];
   }
@@ -79,15 +77,15 @@ function MergeCard(card) {
   for (let i = 0, width = 0, height = 0; i < card.length; i++) {
     let image_tmp = images(card[i].name);
 
-    if (MergeImages === undefined) 
+    if (MergeImages === undefined)
       MergeImages = images(image_tmp.width() * (card.length < 5 ? card.length + 1 : 5) + card.length * space, card.length >= 5 ? image_tmp.height() * 2 + space : image_tmp.height())
-    
+
     if (i === 5) {
       width = 0;
       height = image_tmp.height() + space;
       MergeImages.draw(image_tmp, width, height);
       width += image_tmp.width() + space;
-    } 
+    }
     else {
       MergeImages.draw(image_tmp, width, height);
       width += image_tmp.width() + space;
@@ -97,7 +95,7 @@ function MergeCard(card) {
 }
 
 function setCardPool(index) {
-  let i = index-1;
+  let i = index - 1;
   let path_Bo_SSR = "";
   let path_Bo_SR = "";
   let cardpool = {
@@ -145,12 +143,11 @@ function setCardPool(index) {
 function getAllFileName(path) {
   let array = [];
   fs.readdirSync(path).forEach(file => {
-    if (file !== undefined) 
+    if (file !== undefined)
       array.push(path + file);
   });
   return array;
 }
-
 
 function requireUncached(module) {
   delete require.cache[require.resolve(module)];
@@ -172,7 +169,7 @@ function DrawOtionParser(message) {
         Help(message);
         return undefined;
       case 'c':
-        if(N.test(opt.arg) && opt.arg <= drawJson.cardpool.length) drawOpt.pool_index = parseInt(opt.arg);
+        if (N.test(opt.arg) && opt.arg <= drawJson.cardpool.length) drawOpt.pool_index = parseInt(opt.arg);
         else {
           Help(message);
           return undefined;
@@ -182,15 +179,15 @@ function DrawOtionParser(message) {
         if (N.test(opt.arg) && opt.arg <= 10) drawOpt.count = parseInt(opt.arg);
         else {
           Help(message);
-          return undefined ;
+          return undefined;
         }
         break;
       case 'i':
-        if (opt.arg.toLowerCase() === "all" || (N.test(opt.arg) && opt.arg <= drawJson.cardpool.length)) 
+        if (opt.arg.toLowerCase() === "all" || (N.test(opt.arg) && opt.arg <= drawJson.cardpool.length))
           ShowCardPoolInfo(opt.arg);
-        else 
+        else
           Help(message);
-        return undefined ;
+        return undefined;
       default:
         Help(message);
         return undefined;
@@ -200,7 +197,6 @@ function DrawOtionParser(message) {
   return drawOpt;
 }
 
-function ShowCardPoolInfo()
-{
+function ShowCardPoolInfo() {
   ;
 }
