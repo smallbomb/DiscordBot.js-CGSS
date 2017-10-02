1d7a592bceffd6389c5d0312351d243444043eea
smallbomb <rockon590+git@gmail.com>
2017-10-02 16:25:39 +0800

    update ping command
    modify to reply message

--- src/commands/user/ping.js
+++ src/commands/user/ping.js
@@ -1,4 +1,4 @@
 module.exports = async function (client, message) {
-  const m = await message.channel.send("Ping?");
-  m.edit(`${m.createdTimestamp - message.createdTimestamp}ms.`);
+  const m = await message.channel.send("你的聲音我聽見了!");
+  m.edit(`你的聲音我聽見了! 大概在${m.createdTimestamp - message.createdTimestamp}ms後聽見的`);
 };
