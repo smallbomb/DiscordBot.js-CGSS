74b0bf9381d461721f37c1d1365efc5170712918
smallbomb <rockon590+git@gmail.com>
2017-10-03 18:23:47 +0800

    update draw support bounscard

--- src/commands/user/draw.js
+++ src/commands/user/draw.js
@@ -3,6 +3,7 @@ const optset = require('getopt-c');
 const images = require('images');
 const drawJson = requireUncached("../config_json/draw.json");
 const path_root = process.cwd();
+const basicbase = 100 // lowest 0.0x%
 
 module.exports = function (client, message) {
   let card = [];
@@ -11,11 +12,10 @@ module.exports = function (client, message) {
   let userOpt = undefined;
   let count = 0;
   if ((userOpt = DrawOtionParser(message)) === undefined) return;
-  
+
   cardpool = setCardPool(userOpt.pool_index);
-  return ;
-  for (let i = 0; i < userOpt.count; i++) {
-    if (i === 9 && !card.find(c => { return c.type === "SSR" || c.type === "SR" })) // check one SR or SSR at least? 
+  for (let i = 0; i < userOpt.count; i++) { 
+    if (i === 9 && !card.find(c => {return c.type === "SSR" || c.type === "SR"})) // check one SR or SSR at least? 
       card.push(GetACard(cardpool, "SR"));
     else
       card.push(GetACard(cardpool));
@@ -45,32 +45,41 @@ function GetACard(cardpool, str) {
     name: undefined
   };
   let prob = undefined;
-  let SSRprob = parseInt(drawJson.SSRprob) * 100;
-  let SRprob = parseInt(drawJson.SRprob) * 100;
-  let Rprob = parseInt(drawJson.Rprob) * 100;
   if (str === "SSR")
-    prob = Math.floor(Math.random() * SSRprob) + 1;
+    prob = Math.floor(Math.random() * cardpool.SSRprob) + 1;
   else if (str === "SR")
-    prob = Math.floor(Math.random() * SRprob) + SSRprob + 1;
+    prob = Math.floor(Math.random() * cardpool.SRprob) + cardpool.SSRprob + 1;
   else if (str === "R")
-    prob = Math.floor(Math.random() * Rprob) + SSRprob + SRprob + 1;
+    prob = Math.floor(Math.random() * cardpool.Rprob) + cardpool.SSRprob + cardpool.SRprob + 1;
   else
-    prob = Math.floor(Math.random() * (SSRprob + SRprob + Rprob)) + 1;
-  if (prob <= SSRprob) {
-    Card.type = "SSR";
-    Card.name = cardpool.SSR[Math.floor(Math.random() * cardpool.SSR.length)];
+    prob = Math.floor(Math.random() * (cardpool.SSRprob + cardpool.SRprob + cardpool.Rprob)) + 1;
+   
+  if (prob <= cardpool.SSRprob) {
+    if (prob <= cardpool.bo_SSRprob) {
+      Card.type = "bounsSSR";
+      Card.name = cardpool.bo_SSR[Math.floor(Math.random() * cardpool.bo_SSR.length)]; // bounsSSR
+    }
+    else {
+      Card.type = "SSR";
+      Card.name = cardpool.SSR[Math.floor(Math.random() * cardpool.SSR.length)];
+    }
   }
-  else if (prob <= SSRprob + SRprob) {
-    Card.type = "SR";
-    Card.name = cardpool.SR[Math.floor(Math.random() * cardpool.SR.length)];
+  else if (prob <= cardpool.SSRprob + cardpool.SRprob) {
+    if (prob <= cardpool.SSRprob + cardpool.bo_SRprob) {
+      Card.type = "bounsSR";
+      Card.name = cardpool.bo_SR[Math.floor(Math.random() * cardpool.bo_SR.length)]; // bounsSR
+    }
+    else { 
+      Card.type = "SR";
+      Card.name = cardpool.SR[Math.floor(Math.random() * cardpool.SR.length)];
+    }
   }
-  else if (prob <= SSRprob + SRprob + Rprob) {
+  else if (prob <= cardpool.SSRprob + cardpool.SRprob + cardpool.Rprob) {
     Card.type = "R";
     Card.name = cardpool.R[Math.floor(Math.random() * cardpool.R.length)];
   }
   return Card;
 }
-
 
 function MergeCard(card) {
   let space = 20;
@@ -97,11 +106,14 @@ function MergeCard(card) {
 
 function setCardPool(index) {
   let i = index - 1;
-  let path_Bo_SSR = "";
-  let path_Bo_SR = "";
   let cardpool = {
-    BO_SSR: undefined,
-    BO_SR: undefined,
+    SSRprob: parseFloat(drawJson.SSRprob) * basicbase,
+    SRprob: parseFloat(drawJson.SRprob) * basicbase,
+    Rprob: parseFloat(drawJson.Rprob) * basicbase,
+    bo_SSRprob: 0,
+    bo_SRprob: 0,
+    bo_SSR: undefined,
+    bo_SR: undefined,
     SSR: undefined,
     SR: undefined,
     R: undefined
@@ -121,38 +133,47 @@ function setCardPool(index) {
     cardpool.SR = getAllFileName(path_root + drawJson.defaultPath_passion + "SR/");
     cardpool.R = getAllFileName(path_root + drawJson.defaultPath_passion + "R/");
   }
-  else if (drawJson.cardpool[i].type === "Normal") {
-    // cardpool.BO_SSR = getAllFileName(path_root + drawJson.defaultPath_Bo_SSR + "R/");
-    // cardpool.BO_SR = getAllFileName(path_root + drawJson.defaultPath_Bo_SR + "R/");
-    cardpool.SSR = getAllFileName(path_root + drawJson.defaultPath_cute + "SSR/");
-    cardpool.SSR.push.apply(cardpool.SSR, getAllFileName(path_root + drawJson.defaultPath_cool + "SSR/"));
-    cardpool.SSR.push.apply(cardpool.SSR, getAllFileName(path_root + drawJson.defaultPath_passion + "SSR/"));
-    console.log(cardpool.SSR);
-    return ;
-    cardpool.SR = getAllFileName(path_root + drawJson.defaultPath_cute + "SR/");
+  else {
+    if (drawJson.cardpool[i].type === "DoubleSpecial") {
+      cardpool.Rprob -= cardpool.SSRprob;
+      cardpool.SSRprob *= 2;
+    }
+    // === bouns SSR/SR prob ===
+    cardpool.bo_SSRprob = parseFloat(drawJson.cardpool[i].bounsSSRprob) * basicbase; 
+    cardpool.bo_SRprob = parseFloat(drawJson.cardpool[i].bounsSRprob) * basicbase; 
+    // === bouns SR/SR ===
+    cardpool.bo_SSR = getAllFileName(path_root + drawJson.defaultPath_bounsSSR + index + "/");
+    cardpool.bo_SR = getAllFileName(path_root + drawJson.defaultPath_bounsSR + index + "/");
+    // === default SSR ===
+    cardpool.SSR = getAllFileName(path_root + drawJson.defaultPath_cute + "SSR/", cardpool.bo_SSR);
+    cardpool.SSR.push.apply(cardpool.SSR, getAllFileName(path_root + drawJson.defaultPath_cool + "SSR/", cardpool.bo_SSR));
+    cardpool.SSR.push.apply(cardpool.SSR, getAllFileName(path_root + drawJson.defaultPath_passion + "SSR/", cardpool.bo_SSR));
+    // === default SR ===
+    cardpool.SR = getAllFileName(path_root + drawJson.defaultPath_cute + "SR/", cardpool.bo_SR);
+    cardpool.SR.push.apply(cardpool.SR, getAllFileName(path_root + drawJson.defaultPath_cool + "SR/", cardpool.bo_SR));
+    cardpool.SR.push.apply(cardpool.SR, getAllFileName(path_root + drawJson.defaultPath_passion + "SR/", cardpool.bo_SR));
+    // === default R ===
     cardpool.R = getAllFileName(path_root + drawJson.defaultPath_cute + "R/");
-  }
-  else if (drawJson.cardpool[i].type === "Special") {
-    drawJson.defaultPath_cute;
-    drawJson.defaultPath_cool;
-    drawJson.defaultPath_passion;
-    drawJson.defaultPath_Bo_SSR;
-    drawJson.defaultPath_Bo_SR;
-  }
-  else if (drawJson.cardpool[i].type === "DoubleSpecial") {
-    drawJson.defaultPath_cute;
-    drawJson.defaultPath_cool;
-    drawJson.defaultPath_passion;
-    drawJson.defaultPath_Bo_SSR;
-    drawJson.defaultPath_Bo_SR;
+    cardpool.R.push.apply(cardpool.R, getAllFileName(path_root + drawJson.defaultPath_cool + "R/"));
+    cardpool.R.push.apply(cardpool.R, getAllFileName(path_root + drawJson.defaultPath_passion + "R/"));
+    // if (drawJson.cardpool[i].type === "Normal") {
+    // }
+    // else if (drawJson.cardpool[i].type === "Special") {
+    // }
+    // else if (drawJson.cardpool[i].type === "DoubleSpecial") {
+    // }
   }
   return cardpool;
 }
 
-function getAllFileName(path) {
+function getAllFileName(path, cardpool) {
+  // if the cardpool include same SSR/SR(bouns) card, it is not added to array
   let array = [];
   fs.readdirSync(path).forEach(file => {
-    if (file !== undefined)
+    if (file === undefined);
+    else if (cardpool === undefined)
+      array.push(path + file); 
+    else if (!cardpool.find(pool => {return pool.includes(file)})) 
       array.push(path + file);
   });
   return array;
@@ -207,5 +228,18 @@ function DrawOtionParser(message) {
 }
 
 function ShowCardPoolInfo() {
-  ;
+  let info = "```\n";
+  // index
+  info += "卡池編號: " + drawJson.cardpool[i].index + "\n";
+  // name
+  info += "卡池名稱: " + drawJson.cardpool[i].cardname + "\n";
+  // type
+  if (drawJson.cardpool[i].type === "Normal") info += "卡池類型: 常駐\n";
+  else if (drawJson.cardpool[i].type === "Special") info += "卡池類型: 限定\n";
+  else if (drawJson.cardpool[i].type === "DoubleSpecial") info += "卡池類型: 灰限\n";
+  else info += "卡池類型: " + drawJson.cardpool[i].type + "\n";
+  // date
+  info += "開始時間: " + drawJson.cardpool[i].date + "\n";
+  info += "```";
+  message.channel.send(info);
 }
